9fb6b62a7407f4fc4fd8b257490be33d5614d0fc
Konstantin Kulikov <kostya.kulikov20051111@gmail.com>
2024-10-25 10:57:45 +0700

    init

---
+++ lab5/FitnessProgram.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5
+{
+
+    public class FitnessProgram
+    {
+        private string _name;
+        private string _exercisePlan;
+        private int _caloriesBurned;
+        private int _totalExercises;
+        private int _dailyTargetCalories;
+
+        public string Name => _name; 
+        public string ExercisePlan => _exercisePlan; 
+        public int CaloriesBurned => _caloriesBurned; 
+        public int TotalExercises => _totalExercises; 
+        public int DailyTargetCalories => _dailyTargetCalories; 
+
+        public string FitnessLevel
+        {
+            get
+            {
+                if (_dailyTargetCalories > 2000)
+                    return "Спортсмен";
+                else if (_dailyTargetCalories > 1000)
+                    return "Активный";
+                else
+                    return "Новичок";
+            }
+        }
+
+        
+        
+        public FitnessProgram(string name)
+        {
+            _name = name;
+            _exercisePlan = "Не установлен";
+            _caloriesBurned = 0;
+            _totalExercises = 0;
+            _dailyTargetCalories = 0;
+        }
+
+        public void CompleteExercise(int calories)
+        {
+            _caloriesBurned += calories;
+            _totalExercises++;
+        }
+
+        public void ChangeExercisePlan(string newPlan)
+        {
+            _exercisePlan = newPlan;
+        }
+
+        public void AddToExercisePlan(string additionalInfo)
+        {
+            _exercisePlan += ", " + additionalInfo;
+        }
+
+        public void SetDailyTargetCalories(int target)
+        {
+            _dailyTargetCalories = target;
+        }
+        public bool Warning => _caloriesBurned > _dailyTargetCalories; 
+
+        public string Motivate()
+        {
+            string[] messages = {
+            "Ты сможешь!",
+            "Продолжай в том же духе!",
+            "У тебя все получится",
+            "Не останавливайся!",
+        };
+            Random rand = new Random();
+            return messages[rand.Next(messages.Length)];
+        }
+    }
+}
+
---
+++ lab5/Menu.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5
+{
+
+    class Menu
+    {
+        static void Main(string[] args)
+        {
+            FitnessProgram fitnessProgram = null;
+            bool exit = true;
+
+            while (exit)
+            {
+                Console.WriteLine("Выберите действие:");
+                Console.WriteLine("1. Задать имя и план тренировок");
+                Console.WriteLine("2. Установить целевую норму калорий");
+                Console.WriteLine("3. Задать новый план тренировок");
+                Console.WriteLine("4. Поменять план тренировок");
+                Console.WriteLine("5. Выполнить упражнение");
+                Console.WriteLine("6. Вывести текущую информацию об уровне физической подготовки");
+                Console.WriteLine("7. Вывести мотивационное сообщение");
+                Console.WriteLine("8. Выход");
+                Console.Write("Введите номер действия: ");
+                Console.WriteLine("\n  ");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        if (fitnessProgram == null)
+                        {
+                            Console.Write("Введите имя пользователя: ");
+                            string name = Console.ReadLine();
+                            fitnessProgram = new FitnessProgram(name);
+                            Console.Write("Введите план тренировок: ");
+                            fitnessProgram.ChangeExercisePlan(Console.ReadLine());
+                        }
+                        else
+                        {
+                            Console.WriteLine("Имя уже задано и не может быть изменено.");
+                        }
+                        break;
+                    case "2":
+                        Console.Write("Введите целевую норму калорий: ");
+                        int targetCalories = int.Parse(Console.ReadLine());
+                        fitnessProgram.SetDailyTargetCalories(targetCalories);
+                        break;
+                    case "3":
+                        Console.Write("Введите новый план тренировок: ");
+                        fitnessProgram.ChangeExercisePlan(Console.ReadLine());
+                        break;
+                    case "4":
+                        Console.Write("Введите информацию для добавления в план тренировок: ");
+                        fitnessProgram.AddToExercisePlan(Console.ReadLine());
+                        break;
+                    case "5":
+                        Console.Write("Введите количество сожженных калорий: ");
+                        int calories = int.Parse(Console.ReadLine());
+                        fitnessProgram.CompleteExercise(calories);
+                        break;
+                    case "6":
+                        Console.WriteLine($"Уровень физической подготовки: {fitnessProgram.FitnessLevel}");
+                        Console.WriteLine($"Предупреждение: {(fitnessProgram.Warning ? "Цель достигнута" : "Цель не достигнута")}");
+                        Console.WriteLine($"Текущий план тренировок: {fitnessProgram.ExercisePlan}");
+                        Console.WriteLine($"Количество тренировок: {fitnessProgram.TotalExercises}");
+                        break;
+                    case "7":
+                        Console.WriteLine(fitnessProgram.Motivate());
+                        break;
+                    case "8":
+                        exit = false;
+                        break;
+                    default:
+                        Console.WriteLine("Некорректный ввод, попробуйте снова.");
+                        break;
+                }
+                Console.WriteLine("\n  " );
+            }
+        }
+    }
+
+}
